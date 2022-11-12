9c290c2a32dbcd89112578720d51cbe6e9d963bf
maks075 <shlapak.maksym@gmail.com>
2022-11-12 01:39:26 -0600

    added form and respond to form function

--- src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
+++ src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -2,6 +2,8 @@ package org.launchcode.skillstracker.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -17,7 +19,7 @@ public class SkillsController {
                         "<ol>" +
                             "<li>Java</li>" +
                             "<li>JavaScript</li>" +
-                            "<li>Python</li>" +
+                            "<li>Ruby</li>" +
                          "</ol>" +
             "</body>" +
             "</html>";
@@ -28,20 +30,47 @@ public class SkillsController {
 public String form() {
         return "<html>" +
                 "<body>" +
-                "<form action='hello' method='post'>" +  //submit a request to /hello
-                "<label>Name<input type='text' name='name'></label>" +
-                "<select name='language'>My favorite language:" +
-                "<option value='English'>English</option>" +
-                "<option value='Ukrainian'>Ukrainian</option>" +
-                "<option value='German'>German</option>" +
-                "<option value='French'>French</option>" +
-                "<option value='Spanish'>Spanish</option>" +
-                "</select>" +
-                "<input type='submit' value='Greet me!'>" +
+                "<form method='post'>" +  //submit a request to /hello
+                "<label>Name:</label><br /><input type='text' name='name'><br />" +
+                "<label>My first language of choice:<br>" +
+                "<select name='firstlang'>" +
+                "<option value='java'>Java</option>" +
+                "<option value='javascript'>JavaScript</option>" +
+                "<option value='ruby'>Ruby</option>" +
+                "</select></label><br />" +
+                "<label>My second favorite language<br />" +
+                "<select name='secondlang'>" +
+                "<option value='java'>Java</option>" +
+                "<option value='javascript'>JavaScript</option>" +
+                "<option value='ruby'>Ruby</option>" +
+                "</select></label><br />" +
+                "<label>My third favorite language<br />" +
+                "<select name='thirdlang'>" +
+                "<option value='java'>Java</option>" +
+                "<option value='javascript'>JavaScript</option>" +
+                "<option value='ruby'>Ruby</option>" +
+                "</select></label><br />" +
+                "<input type='submit' value='Submit'>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
 }
+
+    @PostMapping("form")
+    @ResponseBody
+    public String respondToFormSubmision(@RequestParam String name, @RequestParam String firstlang, @RequestParam String secondlang, @RequestParam String thirdlang) {
+        return "<html>" +
+                "<body>" +
+                "<h1>" + name + "</h1>" +
+                "<ol>" +
+                "<li>" + firstlang + "</li>" +
+                "<li>" + secondlang + "</li>" +
+                "<li>" + thirdlang + "</li>" +
+                "</ol>" +
+                "</body>" +
+                "</html>";
+    }
+
 
 
 }
